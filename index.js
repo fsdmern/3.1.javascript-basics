abada527f2b15f9eaf2bbf8f59de7a3aa8bdac5d
fsdmern <fsd.mern@gmail.com>
2022-10-04 11:52:40 +0530

    arranged js basics into sections

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //Browser Console
+document.write('<h3>2. console.log()</h3>');
 console.log('I am Java Script and Writing to the Browser Console');
 
 //JS - Alerts
+document.write('<h3>3. Java Script Alerts</h3>');
 // alert('Welcome to Java Script');
 // alert(1 + 2 + 3);
 
 //Java Script Variables (var, let, const)
+document.write('<h3>4. Java Script Variables</h3>');
 //var - can be re-assigned
+document.write('<h3>4a. var - global scope</h3>');
 //Global Scope
 var num1 = 45;
 var num2 = 36;
@@ -26,7 +30,10 @@ document.write('My Name is: ' + name);
 
 //let - It can't be re-declared, but can be re-assigned
 //Block Scope
-
+document.write('<h3>4b. let - block scope</h3>');
+document.write(
+  "<h4>let - It can't be re-declared, but can be re-assigned</h4>"
+);
 let price = 55.5;
 document.write('Price is: ' + price + '<br/>');
 price = 60;
@@ -34,8 +41,10 @@ document.write('Price is: ' + price + '<br/>');
 
 //const - cant be redeclared & cant be re-assigned
 // Block Scope
+document.write('<h3>4c. const - block scope</h3>');
 const foot = 12;
 
+document.write('<h3>5. Assignment Operators</h3>');
 //Assignment Operators
 // = (x=y), += (usage: x+=y => x=x+y), -+ (x-=y => x=x-y), *=, /=. %=, **=
 
@@ -44,11 +53,16 @@ x -= 5;
 document.write('Value of x now is: ' + x + '<br/>');
 
 //Precedence
+document.write('<h3>6. Precendce - Assignment Operators - BODMAS Rule</h3>');
+document.write(
+  '<h4>BODMAS - Bracket Open Division Multiplication Addition Subsctration</h4>'
+);
 //BODMAS - Bracket Open Division Multiplication Addition Subsctration
 let y = 100 + 50 * 3;
 document.write('Value of x now is: ' + y + '<br/>');
 
 // Data Types
+document.write('<h3>7. Data Types</h3>');
 let length = 25;
 let firstname = 'Ramu';
 let obj = {firstName: 'John', lastName: 'Doe'};
@@ -58,64 +72,3 @@ document.write(
 
 let stringnum = 'Mandeep' + 1 + 2 + 3;
 document.write('Value of stringnum now is: ' + stringnum + '<br/>');
-
-//Arrays
-//index value -> starts with 0
-let array1 = ['Ramu', 'Mandeep', 'Saloni'];
-array1[3] = 'Isvitha';
-array1[4] = 'Adiel';
-
-document.write('Value of array: ' + array1 + '<br/>');
-
-let testArray = new Array();
-testArray[0] = 'Apple';
-testArray[1] = 'Umbrella';
-testArray[2] = 'Zebra';
-testArray[3] = 'Cat';
-let len = testArray.length;
-
-document.write('Value of array: ' + testArray + '<br/>');
-
-//Arrays - Methods
-document.write('Length of array: ' + testArray.length + '<br/>');
-document.write('Sort of the array: ' + testArray.sort() + '<br/>');
-document.write('Sort - Reverse of the array: ' + testArray.reverse() + '<br/>');
-document.write('Pop - Cut the last item: ' + testArray.pop() + '<br/>');
-document.write('Push - A new item: ' + testArray.push('Elephant') + '<br/>');
-document.write('Display Array: ' + testArray + '<br/>');
-
-//splice(start,delteCount)
-//splice(start,indextoadd,'newitem')
-let monthArray = ['Jan', 'Mar', 'Apr', 'Jun'];
-document.write(
-  'Display Spliced Array: ' + monthArray.splice(1, 0, 'Feb') + '<br/>'
-);
-document.write('Display Array: ' + monthArray + '<br/>');
-
-document.write(
-  'Display Spliced Array: ' +
-    monthArray.splice(4, 1, 'May', 'Jul', 'Aug') +
-    '<br/>'
-);
-document.write('Display Spliced Array: ' + monthArray + '<br/>');
-
-//Loops
-//For
-let myMonths = ['Jan', 'Mar', 'Apr', 'Jun'];
-document.write('Length: ' + myMonths.length + '<br/>');
-for (i = 0; i < myMonths.length; i++) {
-  document.write('For Loop - This is month - ' + myMonths[i] + '<br />');
-}
-
-//While Loop
-var i = 0,
-  j = 1,
-  k;
-while (i < 30) {
-  document.write('While Loop ' + i + '<br />');
-  k = i + j;
-  i = j;
-  j = k;
-}
-
-
