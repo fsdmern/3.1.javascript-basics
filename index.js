afa6e9bc6996eb5621d8211db90b87a5840d25b1
fsdmern <fsd.mern@gmail.com>
2022-10-08 08:22:07 +0530

    alerts & datatypes covered

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-//Browser Console
+//2. Browser Console
 document.write('<h3>2. console.log()</h3>');
 console.log('I am Java Script and Writing to the Browser Console');
 
-//JS - Alerts
+//3. JS - Alerts
 document.write('<h3>3. Java Script Alerts</h3>');
+//Uncomment while testing
 // alert('Welcome to Java Script');
 // alert(1 + 2 + 3);
+let age = prompt('How old are you ?', 999);
+alert(`You are ${age} years old!!!`);
 
-//Java Script Variables (var, let, const)
+// let isFootBallPlayer = confirm('Are you a football player ?');
+// alert(isFootBallPlayer);
+
+//4. Java Script Variables (var, let, const)
 document.write('<h3>4. Java Script Variables</h3>');
 //var - can be re-assigned
 document.write('<h3>4a. var - global scope</h3>');
@@ -44,15 +50,15 @@ document.write('Price is: ' + price + '<br/>');
 document.write('<h3>4c. const - block scope</h3>');
 const foot = 12;
 
+//5. Assignment Operators
 document.write('<h3>5. Assignment Operators</h3>');
-//Assignment Operators
 // = (x=y), += (usage: x+=y => x=x+y), -+ (x-=y => x=x-y), *=, /=. %=, **=
 
 let x = 20;
 x -= 5;
 document.write('Value of x now is: ' + x + '<br/>');
 
-//Precedence
+//6. Precedence
 document.write('<h3>6. Precendce - Assignment Operators - BODMAS Rule</h3>');
 document.write(
   '<h4>BODMAS - Bracket Open Division Multiplication Addition Subsctration</h4>'
@@ -61,7 +67,8 @@ document.write(
 let y = 100 + 50 * 3;
 document.write('Value of x now is: ' + y + '<br/>');
 
-// Data Types
+//7. Data Types
+//Naming Convention: camelCase
 document.write('<h3>7. Data Types</h3>');
 let length = 25;
 let firstname = 'Ramu';
@@ -72,3 +79,36 @@ document.write(
 
 let stringnum = 'Mandeep' + 1 + 2 + 3;
 document.write('Value of stringnum now is: ' + stringnum + '<br/>');
+
+//Know the Data Type of any value
+document.write('Type of a variable value typeof(5): ' + typeof 5 + '<br/>');
+document.write(
+  'Type of a variable value typeof(15.4): ' + typeof 15.4 + '<br/>'
+);
+document.write(
+  'Type of a variable value typeof("15.4"): ' + typeof '15.4' + '<br/>'
+);
+
+document.write(
+  'Type of a variable value typeof(false): ' + typeof false + '<br/>'
+);
+
+document.write(
+  'Type of a variable value typeof(null): ' + typeof null + '<br/>'
+);
+
+document.write(
+  'Type of a variable value typeof(undefined): ' + typeof undefined + '<br/>'
+);
+
+document.write('Type of a variable value typeof([]): ' + typeof [] + '<br/>');
+document.write('Type of a variable value typeof({}): ' + typeof {} + '<br/>');
+document.write('Type of a variable value typeof(NaN): ' + typeof NaN + '<br/>');
+
+document.write(
+  'Type of a variable value typeof(alert): ' + typeof alert + '<br/>'
+);
+
+document.write(
+  'Type of a variable value typeof(Math): ' + typeof Math + '<br/>'
+);
